9c3c40828b7204bd7cdd1db89782425e79cceae3
albinshiju <albindamn@gmail.com>
2024-11-16 11:54:00 +0530

    changed domain to onrenderdomain

--- src/utils/socket.js
+++ src/utils/socket.js
@@ -1,5 +1,5 @@
 import { io } from 'socket.io-client';
 
-const socket = io('http://localhost:5000'); // Replace with your backend URL
+const socket = io('https://task-management-app-server-vsp6.onrender.com'); // Replace with your backend URL
 
 export default socket;
